4481fc5f605507b12bac5f49f97498020bca10af
Robson Duarte <rduarte@r3wa.com.br>
2015-05-30 13:55:59 -0300

    inital commit

---
+++ r3wa-aws-manager/src/main/java/br/com/r3wa/aws/manager/R3WAAWSManager.java
@@ -0,0 +1,9 @@
+package br.com.r3wa.aws.manager;
+
+public class R3WAAWSManager {
+
+	public void temp() {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ r3wa-aws-manager/src/test/java/br/com/r3wa/aws/manager/R3WAAWSManagerTest.java
@@ -0,0 +1,22 @@
+package br.com.r3wa.aws.manager;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class R3WAAWSManagerTest {
+	
+	
+	private R3WAAWSManager r3waAWSManager;
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		r3waAWSManager.temp();
+	}
+
+}
